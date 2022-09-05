5680f4592bb571cf72d17a8bcbaecbf5076cee46
Joel Hainley <joel@jwbsolutions.com>
2022-09-05 09:48:45 -0700

    support reading from stdin as well as the terminal

--- shorten.go
+++ shorten.go
@@ -1,29 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("shorten : shortens passed in argument\n")
-		fmt.Printf("usage: shorten <word to shorten>\n")
-		fmt.Printf("v0.0.2\n")
-		os.Exit(1)
-	}
 
-	input := os.Args[1]
+	stat, _ := os.Stdin.Stat()
 
-	if len(input) > 2 {
-		firstChar := input[0:1]
-		lastChar := input[len(input)-1:]
-		abbrLen := len(input) - 2
-
-		fmt.Printf("%s%d%s\n", firstChar, abbrLen, lastChar)
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		// piped
+		fmt.Printf("we got it from the stream!!!")
+		processPipe()
 	} else {
-		fmt.Printf("%s\n", input)
+		// from terminal
+
+		if len(os.Args) != 2 {
+			fmt.Printf("shorten : shortens passed in argument\n")
+			fmt.Printf("usage: shorten <word to shorten>\n")
+			fmt.Printf("v0.0.3\n")
+			os.Exit(1)
+		}
+
+		output := shorten(os.Args[1])
+
+		fmt.Printf(output)
 	}
 
 	os.Exit(0)
+}
+
+func processPipe() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Printf(shorten(scanner.Text()))
+	}
+}
+
+/*
+ * does the shortening of input returning a shortened string (if it can shorten it)
+ */
+// TODO : could the implementation/logic be improved?
+func shorten(input string) string {
+	if len(input) <= 2 {
+		return input
+	}
+
+	firstChar := input[0:1]
+	lastChar := input[len(input)-1:]
+	abbrLen := len(input) - 2
+
+	return fmt.Sprintf("%s%d%s\n", firstChar, abbrLen, lastChar)
 }
